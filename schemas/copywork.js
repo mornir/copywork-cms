1e45ce0a08fadb51d9701a9a5c7a6e7265a207a5
mornir <jeromepott@hotmail.com>
2018-12-09 10:57:31 +0100

    Fist commit

---
+++ schemas/copywork.js
@@ -0,0 +1,62 @@
+export default {
+  title: 'Copywork',
+  name: 'copywork',
+  type: 'document',
+  fields: [
+    {
+      title: 'Title',
+      name: 'title',
+      type: 'string',
+      validation: Rule => Rule.required(),
+    },
+    {
+      title: 'Slug',
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 200, // will be ignored if slugify is set
+        slugify: input =>
+          input
+            .toLowerCase()
+            .replace(/\s+/g, '-')
+            .slice(0, 200),
+      },
+    },
+    {
+      title: 'Month and Year',
+      name: 'date',
+      type: 'datetime',
+      validation: Rule => Rule.required(),
+    },
+    {
+      title: 'Logo',
+      name: 'logo',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      title: 'Main Color',
+      name: 'color',
+      type: 'string',
+      validation: Rule => Rule.regex(/^#\S{6}$/, 'Not a valid HEX code'),
+    },
+    {
+      title: 'ULR of copied page',
+      name: 'copiedURL',
+      type: 'url',
+    },
+    {
+      title: 'Video of copied page',
+      name: 'video',
+      type: 'url',
+    },
+    {
+      title: 'CodePen',
+      name: 'codepen',
+      type: 'string',
+    },
+  ],
+}
