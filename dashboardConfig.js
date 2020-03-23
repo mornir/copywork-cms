8a3baf63bcc09dc59349bd35dcdc2b58df763a25
mornir <jeromepott@hotmail.com>
2020-03-23 14:56:39 +0100

    Add Netlify dashboard plugin

---
+++ dashboardConfig.js
@@ -0,0 +1,24 @@
+export default {
+  widgets: [
+    {
+      name: 'netlify',
+      layout: {
+        width: 'medium',
+        height: 'medium',
+      },
+      options: {
+        title: 'Webseite neu generieren',
+        description:
+          'Nach Anpassungen muss die Seite neu aufgebaut werden, damit alle neuen Inhalte angezeigt werden. Dauert ca. 2 Minuten.',
+        sites: [
+          {
+            buildHookId: '5e78bffc9e337d02228afa59',
+            title: 'https://copywork.netlify.com',
+            name: 'copywork',
+            apiId: 'eeec1609-923c-4ac8-9822-a4661b2c8953',
+          },
+        ],
+      },
+    },
+  ],
+}
