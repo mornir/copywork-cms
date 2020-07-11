e1401c9ab65c5147563ae96d096d081113815b2f
mornir <jeromepott@hotmail.com>
2020-07-11 15:27:20 +0200

    Fix cache flag

--- dashboardConfig.js
+++ dashboardConfig.js
@@ -11,7 +11,8 @@ export default {
         description: 'Trigger a rebuild of the website',
         sites: [
           {
-            buildHookId: '5e78bffc9e337d02228afa59?Sanity+use_cache',
+            buildHookId:
+              '5e78bffc9e337d02228afa59?trigger_title=Sanity+use_cache',
             title: 'https://copywork.netlify.com',
             name: 'copywork',
             apiId: 'eeec1609-923c-4ac8-9822-a4661b2c8953',
